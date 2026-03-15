79dfcaac0a5dcae5679f5da72f8f6bb62290e363
Clément DROUET <drouet.clement02@gmail.com>
2026-03-15 18:51:45 +0100

    create table users

---
+++ database/init.sql
@@ -0,0 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
